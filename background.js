c57b9651759ff045d12e3547de82c7d21fac769a
Saheb <saheb210692@gmail.com>
2017-07-05 12:17:21 +0100

    Cleanup and fixing context menu popup

--- background.js
+++ background.js
@@ -1,28 +1,3 @@
-// function getSelectedIdea() {
-//     var text = "";
-//     if (window.getSelection) {
-//         text = window.getSelection().toString();
-//     } else if (document.selection && document.selection.type != "Control") {
-//         text = document.selection.createRange().text;
-//     }
-//     return text;
-// }
-
-// function saveIdea(ideaText) {
-//         // Get a value saved in a form.
-//         var theValue = ideaText;
-//         // Check that there's some code there.
-//         if (!theValue) {
-//           message('Error: No value specified');
-//           return;
-//         }
-//         // Save it using the Chrome extension storage API.
-//         chrome.storage.sync.set({'value': theValue}, function() {
-//           // Notify that we saved.
-//           message('Idea saved!');
-//         });
-//       }
-
 //A generic onclick callback function.
 function genericOnClick(info, tab) {
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -30,7 +5,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     console.log(response.farewell);
   });
 });
-// }
+}
 
-chrome.contextMenus.create({"title" : "Pick Idea!", "onclick" : genericOnClick, "contexts" : ["page","selection","link","editable","image","video",
+chrome.contextMenus.create({"title" : "Pick Idea", "onclick" : genericOnClick, "contexts" : ["page","selection","link","editable","image","video",
                 "audio"]});
